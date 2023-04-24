9abadb331434a5467b0bc5f705421b5477424da0
JJBonete <0812Neutral@gmail.com>
2023-04-24 22:32:37 +0800

    adding auth routes

---
+++ backend/routes/auth.js
@@ -0,0 +1,45 @@
+const User = require("../models/users");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+
+
+// Register a new user
+router.post("/register", async(req, res) => {
+    try {
+        // Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        // Create a new user
+        const newUser = new User({
+            username: req.body.username,
+            password: hashedPassword,
+        });
+
+        // Save the user and respond with the new user object
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Login a user
+router.post("/login", async(req, res) => {
+    try {
+        // Find the user by username
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(400).json("Wrong credentials!");
+
+        // Check the password
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json("Wrong credentials!");
+
+        // Respond with the user object
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
